ae4ca4cebcb229b65992bec1bbe724dc15cc942e
José Cuacés <dfr3sh1@protonmail.com>
2022-10-19 16:24:26 +0200

    Initial Commit

---
+++ Alfred.é/Alfred_e_App.swift
@@ -0,0 +1,17 @@
+//
+//  Alfred_e_App.swift
+//  Alfred.é
+//
+//  Created by Jose Cuaces on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Alfred_e_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
